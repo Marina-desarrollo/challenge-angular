c97c5fae9c321115b4b6468e2be93471d2ddf23b
Marina <mmdelachica@gmail.com>
2026-04-30 20:05:50 +0200

    feat: scaffold home and footer components

--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'main[app-home]',
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
