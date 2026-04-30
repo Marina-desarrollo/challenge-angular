83e0cf54da577431c100826ba5a6a940d57bb49d
Marina <mmdelachica@gmail.com>
2026-04-30 20:03:59 +0200

    feat: changes on button component

--- frontend/src/app/shared/components/button/button.ts
+++ frontend/src/app/shared/components/button/button.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'button[app-button]',
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
     '[class]': '"btn btn--" + variant + " btn--" + size',
     '[disabled]': 'disabled || null',
   },
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
